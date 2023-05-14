601f4157f808b5662833c80420171aa1732ef026
Martin老師 <tpemartin@gmail.com>
2023-05-14 14:39:32 +0800

    clean up and build

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as React from 'react';
+import { Main } from "./components/Layout2";
+import { NavbarCustom, NavbarItem } from "./components/NavbarCustom";
 import './App.css'
-import { Main, Navbar } from './components/Layout'
+import PlayIcon from '@mui/icons-material/PlayArrow';
+import NextIcon from '@mui/icons-material/FastForward';
+import PreviousIcon from '@mui/icons-material/FastRewind';
+import Card from './components/Card';
+import { IconButton } from '@mui/material';
+import dataJson from "./data.json"
+import { Gallery, GalleryItem } from './components/Layout3';
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 
-function App() {
-  
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-  return (
-    <div className="App">
-      <Main>
-        
-      </Main>
-      <Navbar/>
-    </div>
-  )
+export default function App() {
+
+    console.log(dataJson)
+    const users = convertUsersToArrayOfObject(dataJson)
+
+    const style = {margin: "auto", width: "370px"};
+
+    const cards = users.map((e, i) => {
+        return (
+            <GalleryItem key={i}>
+                {/* <Item sx={{height: "300px"}}/> */}
+                <Card user={e} style={style}/>
+            </GalleryItem>
+        )
+    })
+
+    console.log(style)
+    console.log(users[0])
+    return (
+        <div className="App">
+            
+                <Gallery>
+                {cards}
+                </Gallery>
+            
+            <NavbarCustom>
+                <NavbarItem>
+                    <IconButton>
+                        <PreviousIcon />
+                    </IconButton>
+                </NavbarItem>
+                <NavbarItem>
+                    {/* <PlayIcon/> */}
+                    <IconButton>
+                        <PlayIcon />
+                    </IconButton>
+                </NavbarItem>
+                <NavbarItem>
+                    <IconButton>
+                        <NextIcon />
+                    </IconButton>
+                </NavbarItem>
+            </NavbarCustom>
+
+        </div>
+    )
 }
 
-export default App
+function convertUsersToArrayOfObject(users) {
+    const pos = getUserPositionIndex(users[0])
+
+    const usersArrayOfObject = users.map(
+        (user) => {
+            return getUser(user, pos)
+        }
+    )
+    usersArrayOfObject.shift()
+    return usersArrayOfObject
+}
+
+function getUser(targetUser, pos) {
+    return (
+        {
+            name: targetUser[pos.name],
+            grade: targetUser[pos.grade],
+            imgSrc: targetUser[pos.imgSrc],
+            wish: targetUser[pos.wish]
+        }
+    )
+}
+function getUserPositionIndex(user0) {
+    var pos = Object()
+    user0.map(
+        (e, i) => {
+            pos[e] = i
+        }
+    )
+    pos.imgSrc = pos.ThumbnailLink
+    pos.name = pos.Name
+    pos.grade = pos.Grade
+    pos.wish = pos.Wish
+    return pos
+}
+
+function test(){
+    user0 = ['Timestamp', 'Email Address', 'Name', 'Photo', 'Grade', 'Wish', 'ThumbnailLink']
+    var pos = Object()
+    user0.map(
+        (e, i) => {
+            pos[e] = i
+        }
+    )
+    pos.imgSrc = pos.ThumbnailLink
+    pos.name = pos.Name
+    pos.grade = pos.Grade
+    pos.wish = pos.Wish
+    return pos
+}
